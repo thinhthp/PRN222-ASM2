a3cc16cfbebe2cf1f86a2ff8edb38cc330e195c6
thinhthp <thinhthp1@gmail.com>
2025-06-16 13:25:18 +0700

    Initial push

---
+++ zQuitSmoking.Repositories.ThinhTHP/UserNotificationThinhThpRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zQuitSmoking.Repositories.ThinhTHP.Basic;
+using zQuitSmoking.Repositories.ThinhTHP.DBContext;
+using zQuitSmoking.Repositories.ThinhTHP.Models;
+
+namespace zQuitSmoking.Repositories.ThinhTHP
+{
+    public class UserNotificationThinhThpRepository : GenericRepository<UserNotificationThinhThp>
+    {
+        public UserNotificationThinhThpRepository() { }
+
+        public UserNotificationThinhThpRepository(SE18_PRN222_SE1809_G6_QuitSmokingDBContext context) => _context = context;
+
+        public async Task<List<UserNotificationThinhThp>> GetAllAsync()
+        {
+            var item = await _context.UserNotificationThinhThps
+                .Include(x => x.NotificationThinhThp).Include(x => x.UserAccount)
+                .ToListAsync();
+            return item ?? new List<UserNotificationThinhThp>();
+        }
+
+        public new async Task<UserNotificationThinhThp> GetByIdAsync(int id)
+        {
+            var item = await _context.UserNotificationThinhThps
+                .Include(x => x.NotificationThinhThp).Include(x => x.UserAccount)
+                .FirstOrDefaultAsync(x => x.UserNotificationThinhThpid == id);
+            return item ?? new UserNotificationThinhThp();
+        }
+
+        public async Task<List<UserNotificationThinhThp>> SearchAsync(string message, DateTime date, int userId)
+        {
+            var items = await _context.UserNotificationThinhThps
+                .Include(x => x.NotificationThinhThp).Include(x => x.UserAccount)
+                .Where(x =>
+                (x.NotificationThinhThp.Message.Contains(message) || string.IsNullOrEmpty(message))
+                && (date == DateTime.MinValue || (x.SentDate.HasValue && x.SentDate.Value.Date == date.Date))
+                && (x.UserAccount.UserAccountId == userId) || userId == 0)
+                .ToListAsync();
+            return items ?? new List<UserNotificationThinhThp>();
+        }
+    }
+}
---
+++ zQuitSmoking.Services.ThinhTHP/IUserNotificationThinhThpService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zQuitSmoking.Repositories.ThinhTHP.Models;
+
+namespace zQuitSmoking.Services.ThinhTHP
+{
+    public interface IUserNotificationThinhThpService
+    {
+        Task<List<UserNotificationThinhThp>> GetAllAsync();
+        Task<UserNotificationThinhThp> GetByIdAsync(int code);
+        Task<List<UserNotificationThinhThp>> SearchAsync(string message, DateTime date, int userId);
+        Task<int> CreateAsync(UserNotificationThinhThp userNotificationThinhThp);
+        Task<int> UpdateAsync(UserNotificationThinhThp userNotificationThinhThp);
+        Task<bool> DeleteAsync(int code);
+    }
+}
